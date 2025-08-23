cd30dc5158936856cb56a4b92faf0c04c295086a
acdevz <amanchandra.me@gmail.com>
2025-08-23 22:49:08 +0530

    demoTestData added. with a catch!

--- src/main/java/org/books/catalogService/demo/TestDataLoader.java
+++ src/main/java/org/books/catalogService/demo/TestDataLoader.java
@@ -20,7 +20,12 @@ public class TestDataLoader {
 
     @EventListener(ApplicationReadyEvent.class)
     public void loadTestData() {
+        if(bookRepository.count() != 0)
+            return;
         bookRepository.save(Book.of("ISBN-978", "The Great Gatsby", "F. Scott Fitzgerald", 10.99));
         bookRepository.save(Book.of("ISBN-973", "To Kill a Mockingbird", "Harper Lee", 12.99));
+        bookRepository.save(Book.of("ISBN-965", "1984", "George Orwell", 14.99));
+        bookRepository.save(Book.of("ISBN-954", "Pride and Prejudice", "Jane Austen", 9.99));
+        bookRepository.save(Book.of("ISBN-932", "Million Dollar Weekend", "Don Pendleton", 7.99));
     }
 }
